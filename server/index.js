232696876930f804dfe7f4ecab93e7c62f639d58
Robin Edmunds <r.edmunds@live.co.uk>
2020-09-15 19:46:13 +0100

    Add cors, body-parser imports to server/index

--- server/index.js
+++ server/index.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const keys = require("./keys");
 
 const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
 const PORT = 8000;
 
 app.get("/", (req, res) => {
